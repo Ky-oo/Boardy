5c52873669820d156cd270374c9f627cfe73c429
Ky-oo <kylian.patry@gmail.com>
2025-12-18 23:26:20 +0100

    Delete dev URL

--- src/utils/api/api.ts
+++ src/utils/api/api.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const baseURL =
-  import.meta.env.VITE_API_BASE_URL?.toString() || "http://localhost:3000";
+const baseURL = import.meta.env.VITE_API_BASE_URL?.toString();
 
 const api = axios.create({
   baseURL,
