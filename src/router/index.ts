f40c4507bf8d1c2295c336ffccb88e675aae5114
SheiEthan <ethan.tillier@gmail.com>
2026-01-01 12:30:53 +0100

    Feat: RGPD pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,8 +14,20 @@ import Category from "@/pages/Category.vue";
 import Profile from "@/pages/Profile.vue";
 import Admin from "@/pages/Admin.vue";
 import Organisation from "@/pages/Organisation.vue";
+import MentionsLegales from "@/pages/MentionsLegales.vue";
+import PolitiqueConfidentialite from "@/pages/PolitiqueConfidentialite.vue";
 
 const routes: RouteRecordRaw[] = [
+      {
+        path: "/politique-confidentialite",
+        name: "politique-confidentialite",
+        component: PolitiqueConfidentialite,
+      },
+    {
+      path: "/mentions-legales",
+      name: "mentions-legales",
+      component: MentionsLegales,
+    },
   {
     path: "/",
     name: "home",
